b154a04ca88d323011d1912deef358eacab569ad
MaximPyanin <maximpyanin@icloud.com>
2024-05-30 01:00:34 +0300

    changed repository layer

--- app/database/repositories/events_repository.py
+++ app/database/repositories/events_repository.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
-from sqlalchemy import insert, update
+from sqlalchemy import insert, update, delete, select, UnaryExpression
+from sqlalchemy.orm import selectinload, joinedload
 
 from app.database.db import DB
 from app.database.models.event import Event
@@ -11,16 +12,37 @@ class EventsRepository:
         self.db = db
         self.model = model
 
-    async def create_event(self, event_data: dict):
+    async def insert_one(self, event_data: dict):
         async with self.db.get_sessionmaker() as session:
             stmt = insert(self.model).values(**event_data)
             res = await session.execute(stmt)
             return res.scalars_one()
 
-    async def update_event(self, new_data: dict, event_id: UUID):
+    async def update_one(self, new_data: dict, event_id: UUID):
         async with self.db.get_sessionmaker() as session:
             stmt = (
                 update(self.model).values(**new_data).where(self.model.id == event_id)
             )
             res = await session.execute(stmt)
             return res.scalars_one()
+
+    async def delete_one(self,event_id: UUID):
+        async with self.db.get_sessionmaker() as session:
+            stmt = delete(self.model).where(self.model.id == event_id)
+            res = await session.execute(stmt)
+            return res.scalars_one()
+
+    async def get_many(self,limit:int,offset:int, sort: UnaryExpression | None,data:dict):
+        async with self.db.get_sessionmaker() as session:
+            stmt = (
+                select(self.model)
+                .options(selectinload(self.model.tag))
+                .options(selectinload(self.model.organizer))
+                .options(joinedload(self.model.feedbacks))
+                .filter_by(**data)
+                .order_by(sort)
+                .offset(offset)
+                .limit(limit)
+            )
+            res = await session.execute(stmt)
+            return res.unique().scalars().all()
--- app/database/repositories/users_repository.py
+++ app/database/repositories/users_repository.py
@@ -1,5 +1,4 @@
 from sqlalchemy import insert
-from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from app.database.db import DB
 from app.database.models.user import User
@@ -10,7 +9,7 @@ class UsersRepository:
         self.db = db
         self.model = model
 
-    async def create_user(self, user_data: dict):
+    async def insert_one(self, user_data: dict):
         async with self.db.get_sessionmaker() as session:
             stmt = insert(self.model).values(**user_data)
             res = await session.execute(stmt)
